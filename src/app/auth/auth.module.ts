3fe42578181a16b80fff196c623f0d2c3fea7562
Dimonikal <dpat20@gmail.com>
2019-12-11 03:37:37 +0300

    Update auth.module.ts

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from "./register/register.component";
 import { AuthService } from './auth.service';
+import { ReactiveFormsModule} from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers:[AuthService]
 })
